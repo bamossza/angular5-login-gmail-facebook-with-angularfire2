fec4110f9c3ba1889778f44e0e665d0b0b8b9a26
ภาณุสิทธิ์ เขื่อนคำ <panusit@gmail.com>
2017-12-25 15:29:30 +0700

    Improve e.g. login gmail success redirect to admin -> dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
@@ -11,12 +12,22 @@ import * as firebase from 'firebase/app';
 export class AppComponent {
   title = 'app';
 
-  constructor(public afAuth: AngularFireAuth) {
+  constructor(public afAuth: AngularFireAuth, private router: Router) {
   }
   login() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    .then((resp) => {
+      if (resp.user.email !== '') {
+        this.router.navigate(['/admin/dashboard']);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      this.router.navigate(['/login']);
+    });
   }
   logout() {
+    this.router.navigate(['/login']);
     this.afAuth.auth.signOut();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { firebaseConfig } from '../environments/firebase.config';
@@ -8,17 +9,26 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
+const appRoutes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'login', component: AppComponent},
+  {path: 'admin/dashboard', component: DashboardComponent},
+  // {path: '**', redirectTo: 'login'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(firebaseConfig, 'auth'), // imports firebase/app needed for everything
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
